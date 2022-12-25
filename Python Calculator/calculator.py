f4ef040efbeb1e0618d513acd1972bc135fc7c43
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2022-12-25 15:07:46 -0600

    Add files via upload

---
+++ Python Calculator/calculator.py
@@ -0,0 +1,36 @@
+def calculator ():
+  while True: 
+   num1 = float(input("Enter the first number: "))
+   num2 = float(input("Enter the second number of the calculation: "))
+   operation = input("Enter the operation (+, -, *, /): ")
+   result = 0
+
+   if operation == '+':
+     result = num1 + num2
+   elif operation == '-':
+     result = num1 - num2
+   elif operation == '*':
+     result = num1 * num2
+   elif operation == '/':
+     result = num1 / num2
+   else:
+     result = "Invalid Operator"
+
+     return result
+
+   runAgain = input("Do you have another calculation? (y/n):  ")
+   if runAgain == 'n':
+     print("Closing")
+     break
+
+calculator()
+
+
+
+
+
+
+
+
+
+
